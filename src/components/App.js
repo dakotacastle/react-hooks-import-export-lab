62004112d9124517e14470a574cc7c72c0ab5cd5
Dakota Castle <barrettcastle@gmail.com>
2021-12-14 12:51:10 -0500

    commit tree 6ea818871ea8f8dd959e1114774f5170453dc013

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import NavBar from "./NavBar"
+import About from "./About"
+import Home from "./Home"
+import { image, username, city } from "../data/user";
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city}/>
+      <About image={image} />
     </div>
   );
 }
